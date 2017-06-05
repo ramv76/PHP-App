03011387d6b8fd46313412530a34ea423a65f3ee
ramv76 <ramv1010@gmail.com>
2017-06-05 10:30:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,4 @@
 
 echo "<h1>Hi, DevOps Team !!!";
 echo "<br>Welcome to OpenShift world";
-echo "<br>Sample CICD Pipeline added";
-echo "<br><br>Just cool and awesome";
-echo "<br><br>This is really fantastic";
-
+echo "<br><br>Welcome Back";
